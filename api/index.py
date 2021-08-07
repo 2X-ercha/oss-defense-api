f6e48ff752b6ecbb196165b74fe0cc2eed0367ce
noionion <noionion@outlook.com>
2021-08-07 12:32:46 +0800

    测试post表单处理

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 import urllib
 import json
+import requests
 
 class handler(BaseHTTPRequestHandler):
 
@@ -12,4 +13,11 @@ class handler(BaseHTTPRequestHandler):
         path = str(self.path)     #获取请求的url
         length = int(self.headers.getheader('content-length'))  #获取除头部后的请求参数的长度
         datas = urllib.urlparse.parse_qs(self.rfile.read(length), keep_blank_values=1)  #获取请求参数数据，请求数据为json字符串
+        self.send_response(200)
+        self.send_header("Content-type","text/html")
+        self.send_header("test",datas)
+        self.end_headers()
         self.wfile.write(json.dumps(datas, ensure_ascii=False).encode())
+
+responce = requests.post("https://oss-defense-api.vercel.app/api")
+print(responce)
