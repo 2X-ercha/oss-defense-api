4ca411adc3fffcb5bc85331871c2e70ead8ef879
noionion <noionion@outlook.com>
2021-08-07 12:36:50 +0800

    测试post表单处理

--- api/index.py
+++ api/index.py
@@ -9,10 +9,9 @@ class handler(BaseHTTPRequestHandler):
         self.send_error(415, 'Only post is supported')
 
     def do_POST(self):
-        enc = "UTF-8"              #设置返回的编码格式
         path = str(self.path)     #获取请求的url
-        length = int(self.headers.getheader('content-length'))  #获取除头部后的请求参数的长度
-        datas = urllib.urlparse.parse_qs(self.rfile.read(length), keep_blank_values=1)  #获取请求参数数据，请求数据为json字符串
+        datas = self.rfile.read(int(self.headers['content-length']))
+        datas = urllib.unquote(datas).decode("utf-8", 'ignore')
         self.send_response(200)
         self.send_header("Content-type","text/html")
         self.send_header("test",datas)
@@ -20,4 +19,4 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(datas, ensure_ascii=False).encode())
 
 responce = requests.post("https://oss-defense-api.vercel.app/api")
-print(responce)
+print(responce.json())
